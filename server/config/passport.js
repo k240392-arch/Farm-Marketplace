bc77d22b48fd61af384a20f44ed1db9dc90a1ef2
k240392-arch <k240392@student.kent.edu.au>
2026-05-25 18:32:38 +1000

    Use GOOGLE_CALLBACK_URL env var for absolute HTTPS callback

--- server/config/passport.js
+++ server/config/passport.js
@@ -53,7 +53,7 @@ passport.use(new GoogleStrategy(
   {
     clientID:     process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:  '/api/auth/google/callback',
+    callbackURL: process.env.GOOGLE_CALLBACK_URL || '/api/auth/google/callback',
     passReqToCallback: true,
   },
   async (req, accessToken, refreshToken, profile, done) => {
